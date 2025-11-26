3594d469db3f61ca738833671e5408626d13987a
Svilsa <SvilsaX@gmail.com>
2025-11-26 20:46:17 +0300

    Задание - Модуль 2. Value Object

---
+++ DeliveryApp.Core/Domain/SharedKernel/Location.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CSharpFunctionalExtensions;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.SharedKernel;
+
+public sealed class Location : ValueObject
+{
+    private const int Min = 1;
+    private const int Max = 10;
+
+    [ExcludeFromCodeCoverage]
+    private Location(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int X { get; }
+    public int Y { get; }
+
+    public static Result<Location, Error> Create(int x, int y)
+    {
+        if (x is < Min or > Max)
+            return new Error("value.is.out.of.range", $"X must be between {Min} and {Max}");
+
+        if (y is < Min or > Max)
+            return new Error("value.is.out.of.range", $"Y must be between {Min} and {Max}");
+
+        return new Location(x, y);
+    }
+
+    /// <summary>
+    ///     Манхэттенское расстояние между двумя точками.
+    /// </summary>
+    public int DistanceTo(Location other)
+    {
+        ArgumentNullException.ThrowIfNull(other);
+
+        return Math.Abs(X - other.X) + Math.Abs(Y - other.Y);
+    }
+
+    public static Location CreateRandom()
+    {
+        var x = Random.Shared.Next(Min, Max + 1);
+        var y = Random.Shared.Next(Min, Max + 1);
+
+        return new Location(x, y);
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return X;
+        yield return Y;
+    }
+
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Core/Domain/SharedKernel/LocationShould.cs
@@ -0,0 +1,84 @@
+﻿using DeliveryApp.Core.Domain.SharedKernel;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Core.Domain.SharedKernel;
+
+public class LocationShould
+{
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(5, 7)]
+    [InlineData(10, 10)]
+    public void BeCorrectWhenParamsAreCorrectOnCreated(int x, int y)
+    {
+        // Arrange
+
+        // Act
+        var location = Location.Create(x, y);
+
+        // Assert
+        location.IsSuccess.Should().BeTrue();
+        location.Value.X.Should().Be(x);
+        location.Value.Y.Should().Be(y);
+    }
+
+    [Theory]
+    [InlineData(0, 5)]
+    [InlineData(11, 5)]
+    [InlineData(5, 0)]
+    [InlineData(5, 11)]
+    public void ReturnErrorWhenParamsAreNotCorrectOnCreated(int x, int y)
+    {
+        // Arrange
+
+        // Act
+        var location = Location.Create(x, y);
+
+        // Assert
+        location.IsSuccess.Should().BeFalse();
+        location.Error.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void BeEqualWhenAllPropertiesIsEqual()
+    {
+        // Arrange
+        var l1 = Location.Create(3, 4).Value;
+        var l2 = Location.Create(3, 4).Value;
+
+        // Act
+        var areEqual = l1.Equals(l2);
+
+        // Assert
+        areEqual.Should().BeTrue();
+    }
+
+    [Fact]
+    public void BeNotEqualWhenOneOfPropertiesIsNotEqual()
+    {
+        // Arrange
+        var l1 = Location.Create(3, 4).Value;
+        var l2 = Location.Create(3, 5).Value;
+
+        // Act
+        var areEqual = l1.Equals(l2);
+
+        // Assert
+        areEqual.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ReturnCorrectManhattanDistance()
+    {
+        // Arrange
+        var from = Location.Create(2, 3).Value;
+        var to = Location.Create(5, 1).Value;
+
+        // Act
+        var distance = from.DistanceTo(to);
+
+        // Assert
+        distance.Should().Be(5, "|5-2| + |1-3| = 3 + 2 = 5");
+    }
+}
