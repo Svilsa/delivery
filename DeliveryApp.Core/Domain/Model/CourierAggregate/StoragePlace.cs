36cb62a342dc76fdc1af02b5f4f5b8114b8dad6b
Svilsa <SvilsaX@gmail.com>
2025-12-03 16:53:29 +0300

    Задание - Модуль 3. Entity

---
+++ DeliveryApp.Core/Domain/Model/CourierAggregate/StoragePlace.cs
@@ -0,0 +1,95 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CSharpFunctionalExtensions;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.Model.CourierAggregate;
+
+public class StoragePlace : Entity<Guid>
+{
+    [ExcludeFromCodeCoverage]
+    private StoragePlace() : base(Guid.CreateVersion7())
+    {
+    }
+
+    [ExcludeFromCodeCoverage]
+    private StoragePlace(string name, int totalVolume) : this()
+    {
+        Name = name;
+        TotalVolume = totalVolume;
+    }
+
+    public string Name { get; private set; }
+    public int TotalVolume { get; }
+    public Guid? OrderId { get; private set; }
+
+    [MemberNotNullWhen(false, nameof(OrderId))]
+    public bool IsEmpty => !OrderId.HasValue;
+
+    public static Result<StoragePlace, Error> Create(string name, int totalVolume)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            return GeneralErrors.ValueIsRequired(nameof(name));
+
+        if (totalVolume <= 0)
+            return GeneralErrors.ValueIsInvalid(nameof(totalVolume));
+
+        return new StoragePlace(name, totalVolume);
+    }
+
+    public Result<bool, Error> CanStore(int orderVolume)
+    {
+        if (orderVolume <= 0)
+            return GeneralErrors.ValueIsInvalid(nameof(orderVolume));
+        if (!IsEmpty)
+            return Errors.StorageIsOccupied();
+        if (orderVolume > TotalVolume)
+            return Errors.TooLargeOrderVolume();
+
+        return true;
+    }
+
+    public UnitResult<Error> StoreOrder(Guid orderId, int orderVolume)
+    {
+        var canStoreResult = CanStore(orderVolume);
+        if (!canStoreResult.IsSuccess)
+            return canStoreResult;
+
+        if (orderId == Guid.Empty)
+            return GeneralErrors.ValueIsRequired(nameof(orderId));
+
+        OrderId = orderId;
+
+        return UnitResult.Success<Error>();
+    }
+
+    public UnitResult<Error> RemoveOrder()
+    {
+        if (IsEmpty)
+            return Errors.StorageIsEmpty();
+
+        OrderId = null;
+
+        return UnitResult.Success<Error>();
+    }
+
+    public static class Errors
+    {
+        public static Error StorageIsEmpty()
+        {
+            return new Error($"{nameof(StoragePlace).ToLowerInvariant()}.is.empty",
+                "The storage is empty");
+        }
+
+        public static Error StorageIsOccupied()
+        {
+            return new Error($"{nameof(StoragePlace).ToLowerInvariant()}.is.occupied",
+                "The storage is taken by another order");
+        }
+
+        public static Error TooLargeOrderVolume()
+        {
+            return new Error(
+                "order.volume.is.too.large", "Storage volume is not enough to store the order volume");
+        }
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Core/Domain/Model/CourierAggregate/StoragePlaceShould.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using DeliveryApp.Core.Domain.Model.CourierAggregate;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Core.Domain.Model.CourierAggregate;
+
+public class StoragePlaceShould
+{
+    [Theory]
+    [InlineData("Рюкзак", 10)]
+    [InlineData("Багажник велосипеда", 15)]
+    [InlineData("Багажник машины", 30)]
+    public void BeCorrectWhenParamsAreCorrectOnCreated(string name, int totalVolume)
+    {
+        // Act
+        var res = StoragePlace.Create(name, totalVolume);
+
+        // Assert
+        res.IsSuccess.Should().BeTrue();
+        res.Value.Should().NotBeNull();
+        res.Value.Name.Should().Be(name);
+        res.Value.TotalVolume.Should().Be(totalVolume);
+        res.Value.IsEmpty.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(null, 5)]
+    [InlineData("", 5)]
+    [InlineData(" ", 5)]
+    [InlineData("Коробка", -5)]
+    public void ReturnErrorWhenParamsAreNotCorrectOnCreated(string name, int totalVolume)
+    {
+        // Act
+        var res = StoragePlace.Create(name, totalVolume);
+
+        // Assert
+        res.IsFailure.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ReturnErrorWhenCanStoreTooLargeOrder()
+    {
+        // Arrange
+        var sp = StoragePlace.Create("Багажник", 35).Value;
+
+        // Act
+        var canPlace = sp.CanStore(40);
+
+        // Assert
+        canPlace.IsSuccess.Should().BeFalse();
+        canPlace.Error.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void ReturnErrorWhenStoreTooLargeOrder()
+    {
+        // Arrange
+        var sp = StoragePlace.Create("Конверт", 2).Value;
+        var orderId = Guid.CreateVersion7();
+        const int orderVolume = 3;
+
+        // Act
+        var result = sp.StoreOrder(orderId, orderVolume);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        sp.IsEmpty.Should().BeTrue();
+        sp.OrderId.Should().BeNull();
+    }
+
+    [Fact]
+    public void PlaceOrderWhenEmptyAndVolumeFits()
+    {
+        // Arrange
+        var sp = StoragePlace.Create("Рюкзак", 8).Value;
+        var orderId = Guid.CreateVersion7();
+        const int orderVolume = 5;
+
+        // Act
+        var result = sp.StoreOrder(orderId, orderVolume);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        sp.IsEmpty.Should().BeFalse();
+        sp.OrderId.Should().Be(orderId);
+    }
+
+    [Fact]
+    public void ReturnErrorWhenPlaceOrderWhenNotEmpty()
+    {
+        // Arrange
+        var sp = StoragePlace.Create("Рюкзак", 8).Value;
+
+        var firstOrder = Guid.CreateVersion7();
+        const int firstOrderVolume = 5;
+
+        var secondOrderId = Guid.CreateVersion7();
+        const int secondOrderVolume = 3;
+
+        // Act
+        sp.StoreOrder(firstOrder, firstOrderVolume);
+        var result = sp.StoreOrder(secondOrderId, secondOrderVolume);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        sp.OrderId.Should().Be(firstOrder);
+    }
+}
